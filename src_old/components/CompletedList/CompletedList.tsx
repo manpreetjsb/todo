867d03dc407c5a23ca91f9711174f78644bd7e9b
manpreet singh <manpreet_jsb@hotmail.com>
2021-07-08 16:11:48 +0200

    removed old src folder

--- src_old/components/CompletedList/CompletedList.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import Box from '@material-ui/core/Box'
-
-const CompletedList: React.FC = () => {
-  return <Box width={1}>sdfsdfsdfsd</Box>
-}
-export default CompletedList
